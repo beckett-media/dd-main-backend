54d01a90c6d2d29a19fda2c45d328d751d3a9dd7
lovebhardwaj <lovebhardwaj@gmail.com>
2020-07-14 17:46:19 -0400

    Ebay test

--- routes/ebayTest.js
+++ routes/ebayTest.js
@@ -7,26 +7,26 @@ const router = express.Router();
 const SimpleLogger = require("../utils/simpleLogger");
 
 router.post("/ebay-accepted", (req, res) => {
-  SimpleLogger.info(req.body);
-  SimpleLogger.info(req);
+  SimpleLogger.info(JSON.stringify(req.body));
+  SimpleLogger.info(JSON.stringify(req));
   return res.send("Accepted");
 });
 
 router.get("/ebay-accepted", (req, res) => {
-  SimpleLogger.info(req.body);
-  SimpleLogger.info(req);
+  SimpleLogger.info(JSON.stringify(req.body));
+  SimpleLogger.info(JSON.stringify(req));
   return res.send("Accepted");
 });
 
 router.post("/ebay-declined", (req, res) => {
-  SimpleLogger.info(req.body);
-  SimpleLogger.info(req);
+  SimpleLogger.info(JSON.stringify(req.body));
+  SimpleLogger.info(JSON.stringify(req));
   return res.send("Declined");
 });
 
 router.get("/ebay-declined", (req, res) => {
-  SimpleLogger.info(req.body);
-  SimpleLogger.info(req);
+  SimpleLogger.info(JSON.stringify(req.body));
+  SimpleLogger.info(JSON.stringify(req));
   return res.send("Declined");
 });
 
