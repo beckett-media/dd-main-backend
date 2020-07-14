5de738818be79c68d398bad39ab0f2aeee9f05b8
lovebhardwaj <lovebhardwaj@gmail.com>
2020-07-14 17:51:12 -0400

    Logging changed for ebay

--- routes/ebayTest.js
+++ routes/ebayTest.js
@@ -7,26 +7,46 @@ const router = express.Router();
 const SimpleLogger = require("../utils/simpleLogger");
 
 router.post("/ebay-accepted", (req, res) => {
-  SimpleLogger.info(JSON.stringify(req.body));
-  SimpleLogger.info(JSON.stringify(req));
+  SimpleLogger.info(
+    JSON.stringify(req.body),
+    JSON.stringify(req),
+    req.query.code
+  );
+  console.log(req.body);
+  console.log(req);
   return res.send("Accepted");
 });
 
 router.get("/ebay-accepted", (req, res) => {
-  SimpleLogger.info(JSON.stringify(req.body));
-  SimpleLogger.info(JSON.stringify(req));
+  SimpleLogger.info(
+    JSON.stringify(req.body),
+    JSON.stringify(req),
+    req.query.code
+  );
+  console.log(req.body);
+  console.log(req);
   return res.send("Accepted");
 });
 
 router.post("/ebay-declined", (req, res) => {
-  SimpleLogger.info(JSON.stringify(req.body));
-  SimpleLogger.info(JSON.stringify(req));
+  SimpleLogger.info(
+    JSON.stringify(req.body),
+    JSON.stringify(req),
+    req.query.code
+  );
+  console.log(req.body);
+  console.log(req);
   return res.send("Declined");
 });
 
 router.get("/ebay-declined", (req, res) => {
-  SimpleLogger.info(JSON.stringify(req.body));
-  SimpleLogger.info(JSON.stringify(req));
+  SimpleLogger.info(
+    JSON.stringify(req.body),
+    JSON.stringify(req),
+    req.query.code
+  );
+  console.log(req.body);
+  console.log(req);
   return res.send("Declined");
 });
 
