a49df7566f01441786072233f08ede922607ea18
lovebhardwaj <lovebhardwaj@gmail.com>
2020-07-20 16:12:04 -0400

    Token expiration increased to hour

--- models/user.js
+++ models/user.js
@@ -194,12 +194,12 @@ userSchema.methods.generateAuthToken = function () {
   const token = jwt.sign(
     { _id: this._id, role: this.role },
     config.get(stringConstants.JWT_PRIATE_KEY),
-    { expiresIn: "10m" }
+    { expiresIn: "60m" }
   );
 
   return {
     token,
-    expiry: moment.utc(moment(Date.now()).add(10, "minutes")).format(),
+    expiry: moment.utc(moment(Date.now()).add(60, "minutes")).format(),
   };
 };
 
