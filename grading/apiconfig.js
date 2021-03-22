daaebb3603aa82b5510095754e9c6fb7dc399fcf
Chahat <chahat@Administrators-MacBook-Air.local>
2021-03-22 22:00:10 +0530

    cd

--- grading/apiconfig.js
+++ grading/apiconfig.js
@@ -1,14 +1,14 @@
 module.exports = {
     center: {
-        url: 'http://15.207.100.192:5000',
+        url: 'http://3.139.34.166:5000',
         point: 'centering_score'
     },
     corner: {
-        url: 'http://15.207.100.192:5000',
+        url: 'http://3.139.34.166:5000',
         point: 'corners_score'
     },
     combined: {
-        url: 'http://15.207.100.192:5000',
+        url: 'http://3.139.34.166:5000',
         point: 'get_score'
     }
 };
