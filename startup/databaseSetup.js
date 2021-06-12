42f32e9e54577c04bcd7acb3848e7a9402aaf944
Rameez Ashraf <51672983+rameezashraf927@users.noreply.github.com>
2021-06-12 15:06:50 +0500

    update database setup

--- startup/databaseSetup.js
+++ startup/databaseSetup.js
@@ -1,9 +1,11 @@
 const questionaireScript = require("../init/initQuestions");
 const subscriptionScript = require("../init/initSubscription");
+const productScript = require("../init/initProduct");
+const gradeScript = require("../init/initGrade");
 
 module.exports = async () => {
-  await questionaireScript();
-  await subscriptionScript();
-  await productScript();
+	await questionaireScript();
+	await subscriptionScript();
+	await productScript();
 	await gradeScript();
 };
