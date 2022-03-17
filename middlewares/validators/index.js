009b939d44e3a6e061a09314d75aa6714e30bc18
Muhammad Waqas Ilyas <qaskhan02@gmail.com>
2022-03-17 21:22:52 +0500

    Promo Api

--- middlewares/validators/index.js
+++ middlewares/validators/index.js
@@ -2,3 +2,4 @@ module.exports.marketingGoHighVal = require("./marketing/go-high-val");
 module.exports.marketingContactUsVal = require("./marketing/contact-us-val");
 module.exports.auctionValidation = require("./auction.validation");
 module.exports.orderValidation = require("./order.validation");
+module.exports.promoValidation = require("./promo.validation");
---
+++ services/promo.service.js
@@ -0,0 +1,65 @@
+const { Promo } = require("../models/promo.model");
+/**
+ * Create a promo
+ * @param {Object} promoBody
+ * @returns {Promise<promo>}
+ */
+const createPromo = async (promoBody) => {
+  const promo = await Promo.create(promoBody);
+  return promo;
+};
+
+const getPromos = async () => {
+  const promos = await Promo.find({ enabled: true, isDeleted: false });
+  return promos;
+};
+
+/**
+ * Get promo by id
+ * @param {ObjectId} id
+ * @returns {Promise<promo>}
+ */
+const getPromoById = async (id) => {
+  const promo = await Promo.findById(id);
+  if (promo?.isDeleted === true) return null;
+  return promo;
+};
+
+/**
+ * Update promo by id
+ * @param {ObjectId} promoId
+ * @param {Object} updateBody
+ * @returns {Promise<promo>}
+ */
+const updatePromoById = async (promoId, updateBody) => {
+  const promo = await getPromoById(promoId);
+  if (!promo) {
+    throw new Error("Promo not found");
+  }
+  Object.assign(promo, updateBody);
+  await promo.save();
+  return promo;
+};
+
+/**
+ * softDelete promo by id
+ * @param {ObjectId} promoId
+ * @returns {Promise<promo>}
+ */
+const softDeletepromoById = async (promoId) => {
+  const promo = await getPromoById(promoId);
+  if (!promo) {
+    throw new Error("Promo not found");
+  }
+  promo.isDeleted = true;
+  await promo.save();
+  return promo;
+};
+
+module.exports = {
+  createPromo,
+  getPromos,
+  updatePromoById,
+  softDeletepromoById,
+  getPromoById,
+};
