5df1507fdbce9e71398985e3eb17ad496b58793a
diode02 <qaskhan02@gmail.com>
2022-05-26 21:10:34 +0500

    added extra message for checking ned of username

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -70,7 +70,9 @@ module.exports = {
         .max(20)
         .message("username should have a maximum length of 20")
         .pattern(/^(?![_.])/)
-        .message("_ or . can't be at the start of a username (e.g _test, .test)")
+        .message("_ or . can't be at the start of a username (e.g _test , .test)")
+        .pattern(/[^._]$/)
+        .message("_ or . can't be at the end of a username (e.g test_  , test.)")
         .pattern(/^(?!.*[_.]{2})/)
         .message(
           "_ or . can't be used multiple times in a row (e.g test__user, test_.user)"
