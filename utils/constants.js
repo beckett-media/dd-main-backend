df94215895eee3ce33c784a8d98c8c53d94fac0e
Muhammad Waqas Ilyas <qaskhan02@gmail.com>
2022-07-28 12:24:28 +0500

    changed message

--- utils/constants.js
+++ utils/constants.js
@@ -128,7 +128,7 @@ module.exports.stringConstants = {
 
   TWITTER_NO_TOKEN: "Token Not found",
 
-  TWITTER_NO_EMAIL: "Twitter sign in email not found. You must provide your email.",
+  TWITTER_NO_EMAIL: "Email is missing on your twitter account. You must add it on your twitter profile to proceed with twitter sign in on Due Dilly.",
 
   APPLE_ID_DOES_NOT_MATCH_EMAIL:
     "User Identifier does not match the given email",
